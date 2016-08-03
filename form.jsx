e93438ea7ac6ba5f94fa0341ee33566a084e66b9
mahong <1025383143@qq.com>
2016-08-04 00:57:08 +0800

    add Editor

--- form.jsx
+++ form.jsx
@@ -6,12 +6,18 @@ var App = React.createClass({
     toogle:function () {
       this.setState({isEditor:!this.state.isEditor})  
     },
+    addElement:function (ele) {
+        this.state.elements.push(ele);
+        this.setState({elements})
+    },
     render: function () {
         const isEditor=this.state.isEditor;
         return (
         <div>
             <button onClick={this.toogle}>{isEditor?'preview':"edit"}</button>
-            <div className={isEditor?'':'hidden'}><Editor/></div>
+            <div className={isEditor?'':'hidden'}>
+                <Editor onAdd={this.addElement} onDelete={this.remove} elements={this.state.elements} />
+            </div>
             <div className={isEditor?'hidden':''}><Preview/></div>
         </div>
 
@@ -22,18 +28,10 @@ var App = React.createClass({
 var Editor = React.createClass({
     render: function () {
         return (
-        <div></div>
-           
-        )
-    }
-});
-
-
-
-var Preview = React.createClass({
-    render: function () {
-        return (
-        <div></div>
+        <div>
+            <Right  onAdd={this.props.onAdd}/>
+            <Left onDelete={this.props.onDelete} elements={this.props.elements}/>
+        </div>
            
         )
     }
